14473437ebc39460004514fd85047fd902c7d409
2Much2Code:) <106218100+AdiNarendra98@users.noreply.github.com>
2022-11-23 11:43:40 +0530

    commit

---
+++ 12. Sea Ice Segementation from R.Sensing Images/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+__version__ = (0, 0, 0)
+
+setup(
+    name="sea_ice_rs",
+    description="Sea Ice Remote Sensing",
+    version=".".join(str(d) for d in __version__),
+    author="Sangwon Lim",
+    author_email="sangwonl@uvic.ca",
+    packages=["sea_ice_rs"],
+    include_package_data=True,
+    scripts="""
+        ./scripts/threshold
+        ./scripts/dist-stat
+        ./scripts/extract-colour
+        ./scripts/connect-lines
+        ./scripts/centroids
+        ./scripts/create-datasets
+        ./scripts/neural-network
+        ./scripts/normalize
+        ./scripts/GLCM
+        ./scripts/CNN
+        ./scripts/test-model
+        ./scripts/predict
+    """.split(),
+)
